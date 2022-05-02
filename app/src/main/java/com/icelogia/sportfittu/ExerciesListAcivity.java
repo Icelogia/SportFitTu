48dd9203bc6303442681cd36f51913fe38c8393b
Icelogia <35977372+Icelogia@users.noreply.github.com>
2022-05-02 20:38:28 +0200

    Activities Main Activity layout

---
+++ app/src/main/java/com/icelogia/sportfittu/ExerciesListAcivity.java
@@ -0,0 +1,14 @@
+package com.icelogia.sportfittu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExerciesListAcivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercies_list_acivity);
+    }
+}
