d9a1f23c06e631c87f8d348c96e3ad83d7eefab1
Icelogia <35977372+Icelogia@users.noreply.github.com>
2022-05-27 21:20:24 +0200

    Dark Mode

--- app/src/main/java/com/icelogia/sportfittu/MainActivity.java
+++ app/src/main/java/com/icelogia/sportfittu/MainActivity.java
@@ -1,11 +1,14 @@
 package com.icelogia.sportfittu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -31,6 +34,44 @@ public class MainActivity extends AppCompatActivity {
         assignButtonToPageNavigation(trainingPlanButton, TrainingPlanActivity.class);
         assignButtonToPageNavigation(exerciseListButton, ExerciesListAcivity.class);
         assignButtonToPageNavigation(calculatorButton, CalculatorActivity.class);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        final boolean isDarkModeOn = sharedPreferences.getBoolean("isDarkModeOn", false);
+
+        // When user reopens the app
+        // after applying dark/light mode
+        if (isDarkModeOn)
+        {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else
+        {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
+        Switch darkModeSwitch = findViewById(R.id.switchDarkMode);
+
+        darkModeSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (darkModeSwitch.isChecked())
+                {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor.putBoolean("isDarkModeOn", false);
+                    editor.apply();
+
+                }
+                else
+                {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor.putBoolean("isDarkModeOn", true);
+                    editor.apply();
+
+                }
+            }
+        });
 
 
         MobileAds.initialize(this);
